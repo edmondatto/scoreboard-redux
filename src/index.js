94ea23f166be764d17adeca570c5867b8af7f48f
Edmond Atto <edmond091@gmail.com>
2017-11-12 15:23:32 +0300

    wrap main app component inside provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Provider from 'react-redux';
+import { createStore } from 'redux';
+import PlayerReducer from 'reducers/player'
 import Scoreboard from './components/layout';
 import PLAYERS from "./components/data";
 
-ReactDOM.render(<Scoreboard initialPlayers={PLAYERS}/>, document.getElementById('root'));
+const store = createStore(
+  PlayerReducer
+);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Scoreboard initialPlayers={PLAYERS}/>
+  </Provider>,
+  document.getElementById('root'));
