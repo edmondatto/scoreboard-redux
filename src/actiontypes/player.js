6e6909b3c612cc120455c9b4d510273a8f80fa36
Edmond Atto <edmond091@gmail.com>
2017-11-12 14:53:33 +0300

    Update action type name

--- src/actiontypes/player.js
+++ src/actiontypes/player.js
@@ -1,3 +1,3 @@
 export const ADD_PLAYER = 'player/ADD_PLAYER';
 export const REMOVE_PLAYER = 'player/REMOVE_PLAYER';
-export const ADD_PLAYER_SCORE = 'player/ADD_PLAYER_SCORE';
+export const UPDATE_PLAYER_SCORE = 'player/UPDATE_PLAYER_SCORE';
